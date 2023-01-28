08aa40c7a4f812ac6a028e6c76691922754928cd
Animesh77666 <animesh77666@gmail.com>
2023-01-28 21:58:11 +0530

    second commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import "./style.css";
 
 document.querySelector("#app").innerHTML = `
   <div>
-    
-    
     <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
@@ -14,4 +12,4 @@ document.querySelector("#app").innerHTML = `
   </div>
 `;
 
-setupCounter(document.querySelector("#counter"));
+
