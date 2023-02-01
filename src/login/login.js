1f193dce0389e7c59b2e8788a791323532a4fda8
Animesh77666 <animesh77666@gmail.com>
2023-02-01 15:56:57 +0530

    test

---
+++ src/login/login.js
@@ -0,0 +1,6 @@
+const CLIENT_ID = "ea0ac49cff2e403ab42acadb5bef115c";
+const scopes = "user-top-read user-follow-read playlist-read-private user-library-read"
+
+document.addEventListener(DOMContentLoaded, ()=> {
+
+})
